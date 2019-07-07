24504b60fd064c382182564cc7abcd72e3a18f6d
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-07-07 14:15:53 +0200

    start with let

--- trial.lisp
+++ trial.lisp
@@ -8,3 +8,33 @@
 
 (setf (readtable-case *readtable*) :invert)
 
+(progn
+  (defparameter *env-functions* nil)
+  (defparameter *env-macros* nil)
+  (defun emit-go (&key code (str nil) (clear-env nil) (level 0))
+					;(format t "emit ~a ~a~%" level code)
+    (when clear-env
+      (setf *env-functions* nil
+	    *env-macros* nil))
+    (flet ((emit (code &optional (dl 0))
+	     (emit-go :code code :clear-env nil :level (+ dl level))))
+      (if code
+	  (if (listp code)
+	      (case (car code)
+		(paren (let ((args (cdr code)))
+			 (format nil "(~{~a~^, ~})" (mapcar #'emit args))))
+		(let (destructuring-bind (args &rest body) (cdr code)
+		       (format t "let ~a" (list args body))))
+		(t (destructuring-bind (name &rest args) code
+		     (format nil "~a~a" name
+			     (emit `(paren ,@args)))
+		     )))
+	      (cond
+		((or (symbolp code)
+		     (stringp code)) ;; print variable
+		 (format nil "~a" code))
+		((numberp code) ;; print constants
+		 (cond ((integerp code) (format str "~a" code))
+		       ))))
+	  "")))
+  (emit-go :code `(let ((a 3)))))
