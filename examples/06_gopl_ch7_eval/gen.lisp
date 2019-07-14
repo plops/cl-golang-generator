affdefcbd2f9bdbbac7d49ad3946124e7da55dc1
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-07-14 19:29:22 +0200

    start eval

---
+++ examples/06_gopl_ch7_eval/gen.lisp
@@ -0,0 +1,16 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-golang-generator"))
+
+(in-package :cl-golang-generator)
+
+(progn
+  (defparameter *path* "/home/martin/quicklisp/local-projects/cl-golang-generator/examples/06_gopl_ch7_eval")
+  (defparameter *code-file* "eval")
+  (defparameter *source* (format nil "~a/source/~a" *path*  *code-file*))
+  (let* ((code
+	  `(do0
+	    (package main)
+	    (import fmt sort))))
+    (write-source *source* code)))
+
+
