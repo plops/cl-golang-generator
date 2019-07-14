96ceee8627ce5eaa6e11038a6826425c6fd3b8c1
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-07-14 09:52:41 +0200

    use macro to write complicated map

--- examples/05_gopl_ch5_toposort/gen.lisp
+++ examples/05_gopl_ch5_toposort/gen.lisp
@@ -11,10 +11,31 @@
 	  `(do0
 	    (package main)
 	    (import fmt sort)
-	    (let ((prereqs (curly "map[string][]string"
-				  :algorithms (string "data structures"))))
-	     (lambda (items)
-	       (declare (type "[]string" items)))))))
+	    (let ((prereqs (cast "map[string][]string"
+				 (dict
+				  ,@(loop for e in `((algorithm (data-structures))
+						     (calculus (linear-algebra))
+						     (compilers (data-structures
+								 formal-languages
+								 computer-organization))
+						     (data-structures (discrete-math))
+						     (databases (data-structures))
+						     (discrete-math (intro-to-programming))
+						     (formal-languages (discrete-math))
+						     (networks (operating-systems))
+						     (operating-systems (data-structures
+									 computer-organization))
+						     (programming-languages (data-structures
+									     computer-organization)))
+				       collect
+					 (destructuring-bind (name components) e
+					   `((string ,name) (braces ,@(mapcar
+								       (lambda (x)
+									 `(string ,x))
+								       components)))))))))
+	      (defun main ()
+		(foreach ((ntuple i course) (range (topoSort prereqs)))
+			 (fmt.Printf (string "%d:\\t%s\\n") (+ i 1) course)))))))
     (write-source *source* code)))
 
 
--- examples/05_gopl_ch5_toposort/source/toposort.go
+++ examples/05_gopl_ch5_toposort/source/toposort.go
@@ -1,8 +1,14 @@
 package main
+
 import (
-"fmt"
-"sort"
+	"fmt"
+	"sort"
 )
-var prereqs = map[string][]string {algorithms: (string data structures)}
-func (items []string) {
+
+var prereqs = map[string][]string{"algorithm": {"data-structures"}, "calculus": {"linear-algebra"}, "compilers": {"data-structures", "formal-languages", "computer-organization"}, "data-structures": {"discrete-math"}, "databases": {"data-structures"}, "discrete-math": {"intro-to-programming"}, "formal-languages": {"discrete-math"}, "networks": {"operating-systems"}, "operating-systems": {"data-structures", "computer-organization"}, "programming-languages": {"data-structures", "computer-organization"}}
+
+func main() {
+	for i, course := range topoSort(prereqs) {
+		fmt.Printf("%d:\t%s\n", ((i) + (1)), course)
+	}
 }
