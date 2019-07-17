16c5f2b1746ade6962358d8a23bbb85b1cf7aac3
Martin Kielhorn <kielhorn.martin@gmail.com>
2019-07-17 09:09:16 +0200

    channel code

---
+++ examples/10_gopl_ch9_concurrent_memoization_channel/gen.lisp
@@ -0,0 +1,84 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-golang-generator"))
+
+(in-package :cl-golang-generator)
+
+(progn
+  (defparameter *path* "/home/martin/quicklisp/local-projects/cl-golang-generator/examples/10_gopl_ch9_concurrent_memoization_channel")
+  (defparameter *code-file* "memo")
+  
+  (defparameter *source* (format nil "~a/source/~a" *path*  *code-file*))
+
+
+  
+  
+  (let* ((code
+	  `(do0
+	    (package main)
+	    "// concurrent memoization using communicating sequential processes"
+	    "// 1) monitor goroutine synchronizes access to map"
+	    "// 2) get sends message with key and response channel"
+	    "// 3a) "
+	    "// 3b) "
+	    
+	    (import sync)
+
+	    (deftype Func ()
+	      (defun-declaration func (key)
+		(declare (type string key)
+			 (values "interface{}"
+				 error))))
+
+	    (defstruct0 result
+		(value "interface{}")
+	      (err error))
+
+	    
+	    
+	    (defstruct0 entry
+		(res result)
+	      (ready "chan struct{}"))
+
+	    (defstruct0 request
+		(key string)
+	      (response "chan<- result"))
+	    
+	    
+	    (defstruct0 Memo
+		(requests "chan request"))
+
+	    (defun New (f)
+	      (declare (type Func f)
+		       (values *Memo))
+	      "// clients have to subsequently call close"
+	      (assign memo 
+		      (curly &Memo
+			     :requests (make "chan request")))
+	      (go (memo.server f))
+	      (return memo))
+	    
+	    (defmethod Get ((memo *Memo) key)
+	      (declare (type string key)
+		       (values "interface{}" error))
+	      (assign response (make "chan resul"))
+	      (<- memo.requests (curly request key response))
+	      (assign res (<- response))
+	      (resturn (ntuple res.value
+			       res.err)))
+	    (defmethod Close ((memo *Memo))
+	      (close memo.requests))
+
+	    (defmethod server ((memo *Memo) f)
+	      (declare (type Func f))
+	      (assign cache (make "map[string]*entry"))
+	      (foreach (req (range memo.requests))
+		       (assign e (aref cache req.key))
+		       (when (== e "nil")
+			 "// first request for key"
+			 (setf e (curly &entry :ready (make "chan struct{}"))
+			       (aref cache req.key) e)
+			 (go (e.call f req.key)))
+		       (go (e.deliver req.response)))))))
+    (write-source *source* code)))
+
+
---
+++ examples/10_gopl_ch9_concurrent_memoization_channel/source/memo.go
@@ -0,0 +1,56 @@
+package main
+
+// concurrent memoization using communicating sequential processes
+// 1) monitor goroutine synchronizes access to map
+// 2) get sends message with key and response channel
+// 3a)
+// 3b)
+import (
+	"sync"
+)
+
+type Func func(key string) (interface{}, error)
+type result struct {
+	value interface{}
+	err   error
+}
+type entry struct {
+	res   result
+	ready chan struct{}
+}
+type request struct {
+	key      string
+	response chan<- result
+}
+type Memo struct {
+	requests chan request
+}
+
+func New(f Func) *Memo {
+	// clients have to subsequently call close
+	memo := &Memo{requests: make(chan request)}
+	go memo.server(f)
+	return memo
+}
+func (memo *Memo) Get(key string) (interface{}, error) {
+	response := make(chan resul)
+	memo.requests <- request{key, response}
+	res := <-response
+	resturn(res.value, res.err)
+}
+func (memo *Memo) Close() {
+	close(memo.requests)
+}
+func (memo *Memo) server(f Func) {
+	cache := make(map[string]*entry)
+	for req := range memo.requests {
+		e := cache[req.key]
+		if (e) == (nil) {
+			// first request for key
+			e = &entry{ready: make(chan struct{})}
+			cache[req.key] = e
+			go e.call(f, req.key)
+		}
+		go e.deliver(req.response)
+	}
+}
