2f69f879facc124ba6ff5ae0ef128d22d4b53acd
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-02-11 00:53:22 +0100

    more code

--- examples/13_net/gen00.lisp
+++ examples/13_net/gen00.lisp
@@ -24,6 +24,55 @@
 			      (setf _ (listener.Close)))))
 		    (te.Logf (string "bound to %q")
 			     (listener.Addr)))
+		  ))
+  (write-source (format nil "~a/source/dial_test" *path*)
+		`(do0
+		  ;; A Woodbeck Network Programming with Go p.53
+		  (package ch03)
+		  (import io
+			  net
+			  testing)
+		  (defun TestDial (te)
+		    (declare (type *testing.T te))
+
+		    (do0
+		     (assign (ntuple listener
+				     err)
+			     (net.Listen (string "tcp")
+					 (string "127.0.0.1:0")))
+		     (unless (== err "nil")
+		       (te.Fatal err)))
+		    (do0
+		     (assign done (make (chan (curly struct)))))
+		    (go ((lambda ()
+			   (defer ((lambda ()
+				     (<- done (curly "struct{}")))))
+			   (for ()
+				(do0
+				 (assign (ntuple conn err)
+					 (listener.Accept))
+				 (unless (== err "nil")
+				   (te.Log err)
+				   return))
+				(go ((lambda (c)
+				       (declare (type net.Conn c))
+				       (defer ((lambda ()
+						 (c.Close)
+						 (<- done (curly "struct{}")))))
+				       (do0
+					(assign buf (make "[]byte" 1024))
+					(for ()
+					     (do0 (assign (ntuple n err)
+							  (c.Read buf))
+						  (unless (== err "nil")
+						    (unless (== err io.EOF)
+						      (te.Error err))
+						    return)
+						  (te.Logf (string "received: %q")
+							   (aref buf (slice "" n))))
+					     ))) conn)))
+			   )))
+		  )
 		  )))
 
 
