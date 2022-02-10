f34fcfff200fd73ab352dbb578e6308ccbffc485
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-02-10 23:35:39 +0100

    change filename

--- examples/13_net/gen00.lisp
+++ examples/13_net/gen00.lisp
@@ -5,24 +5,23 @@
 
 (progn
   (defparameter *path* "/home/martin/quicklisp/local-projects/cl-golang-generator/examples/13_net")
-  (defparameter *code-file* "main")
-
-  (write-source (format nil "~a/source/~a" *path*  *code-file*)
+  (write-source (format nil "~a/source/listen_test" *path*)
 		`(do0
 		  ;; A Woodbeck Network Programming with Go p.51
+		  ;; run with go test listen_test.go
 		  (package ch03)
 		  (import net
 			  testing)
 		  (defun TestListener (te)
 		    (declare (type *testing.T te))
-		    (setf (ntuple listener
+		    (assign (ntuple listener
 				  err)
 			  (net.Listen (string "tcp")
 				      (string "127.0.0.1:0")))
-		    (unless (== err nil)
+		    (unless (== err "nil")
 		      (te.Fatal err))
 		    (defer ((lambda ()
-			      (= _ (listener.Close)))))
+			      (setf _ (listener.Close)))))
 		    (te.Logf (string "bound to %q")
 			     (listener.Addr)))
 		  )))
