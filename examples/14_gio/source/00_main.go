adc250d1409e9e3b4c580073ff716400ff951609
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-06-18 11:22:42 +0200

    more code

---
+++ examples/14_gio/source/00_main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"gioui.org/app"
+	"gioui.org/font/gofont"
+	"gioui.org/io/system"
+	"gioui.org/layout"
+	"gioui.org/op"
+	"gioui.org/text"
+	"gioui.org/widget/material"
+	"image/color"
+	"log"
+	"os"
+)
+
+func main() {
+	go (func() {
+		w := app.NewWindow()
+		err := run(w)
+		if !((err) == (nil)) {
+			log.Fatal(err)
+		}
+		os.Exit(0)
+	})()
+	app.Main()
+}
+func run() error {
+	th := material.NewTheme(gofont.Collection())
+	var ops op.Ops
+	for {
+		ev := <-w.Events()
+		e := ev.(type)
+		switch e {
+		case system.DestroyEvent:
+			return e.Err
+		case system.FrameEvent:
+			gtx := layout.NewCotnext(&ops, e)
+		}
+	}
+}
