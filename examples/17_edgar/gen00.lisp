b12045d580b6ca737459859239286e3d80d7cf8b
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-07-16 00:16:00 +0200

    more code

--- examples/17_edgar/gen00.lisp
+++ examples/17_edgar/gen00.lisp
@@ -12,6 +12,7 @@
   (defparameter *path*
     (format nil "~a/stage/cl-golang-generator/examples/17_edgar"
 	    (user-homedir-pathname)))
+  (defparameter *idx* "00")
   (defun lprint (&key (msg "") vars)
     `(fmt.Printf (string ,(format nil "%v ~a ~{~a=%v~^ ~}\\n" msg vars))
 		 
@@ -26,16 +27,21 @@
 		 ,@vars
 		 ))
   (let ((file-count 0))
-
     (defun write-go (name code)
       (prog1
-	  (write-source (format nil "~a/source/~2,'0d_~a" *path* file-count name)
+	  (write-source (format nil "~a/source~a/~2,'0d_~a"
+				*path* *idx* file-count name)
 			code)
 	(incf file-count))))
 
+  (ensure-directories-exist (format nil "~a/source~a/"
+				    *path*
+				    *idx*))
+  
   ;; overwriting this file makes it necessary to call setup01_tidy again
-  (with-open-file (s (format nil "~a/source/go.mod"
-			     *path*)
+  (with-open-file (s (format nil "~a/source~a/go.mod"
+			     *path*
+			     *idx*)
 		     :direction :output
 		     :if-exists nil
 		     :if-does-not-exist :create)
@@ -47,6 +53,7 @@
      (package main)
      (import fmt
 	     time
+	     github.com/piquette/edgr/core
 	     )
      
      (defun main ()
