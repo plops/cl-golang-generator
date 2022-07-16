53717b73c8e605ff0e64bca686be959bf675569e
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-07-16 06:24:32 +0200

    format string expects a specific time

---
+++ examples/17_edgar/gen01.lisp
@@ -0,0 +1,65 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-golang-generator"))
+
+(in-package :cl-golang-generator)
+
+(progn
+  (defparameter *path*
+    (format nil "~a/stage/cl-golang-generator/examples/17_edgar"
+	    (user-homedir-pathname)))
+  (defparameter *idx* "01")
+  (defun lprint (&key (msg "") vars)
+    `(fmt.Printf (string ,(format nil "%v ~a ~{~a=%v~^ ~}\\n" msg vars))
+		 
+		 (dot time
+		      (Now)
+		      (Format
+		       time.StampMilli
+		       #+nil (string "Jan _2 15:04:05.000"))
+		      #+nil (Format
+		       (string
+			"2017-09-07 17:06:04"
+					;"2017-09-07 17:06:04.000000"
+			)))
+		 
+		 ,@vars
+		 ))
+  (let ((file-count 0))
+    (defun write-go (name code)
+      (prog1
+	  (write-source (format nil "~a/source~a/~2,'0d_~a"
+				*path* *idx* file-count name)
+			code)
+	(incf file-count))))
+
+  (ensure-directories-exist (format nil "~a/source~a/"
+				    *path*
+				    *idx*))
+  
+  ;; overwriting this file makes it necessary to call setup01_tidy again
+  (with-open-file (s (format nil "~a/source~a/go.mod"
+			     *path*
+			     *idx*)
+		     :direction :output
+		     :if-exists nil
+		     :if-does-not-exist :create)
+    (format s "module edgar~%")
+    (format s "go 1.18~%"))
+  (write-go
+   "main"
+   `(do0
+     (package main)
+     (import fmt
+	     time
+	     
+	     )
+     
+     (defun main ()
+       ,(lprint :msg "main")
+
+       #+nil
+       (unless (== err "nil")
+	 (panic err))
+       ,(lprint :msg "" :vars `())
+       )
+     )))
