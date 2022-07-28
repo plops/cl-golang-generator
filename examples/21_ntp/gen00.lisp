01e62afb8a7ebaaa0410e9edd3da19634c0d4dee
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-07-28 19:32:11 +0200

    start with ntp example

---
+++ examples/21_ntp/gen00.lisp
@@ -0,0 +1,136 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-golang-generator"))
+
+(in-package :cl-golang-generator)
+
+(progn
+  (defparameter *path*
+    (format nil "~a/stage/cl-golang-generator/examples/20_pentest"
+	    (user-homedir-pathname)))
+  (defparameter *idx* "00")
+  (defun lprint-init ()
+    `(defun timeNow ()
+       (declare (values string))
+       (return
+	 (dot time
+	     (Now)
+	     (Format
+	      (string
+	       "2006-01-02 15:04:05.000"))))))
+  (defun lprint (&key (msg "") vars)
+    "generate go code to print variables in log output"
+    `(fmt.Printf
+      (string
+       ,(format nil "%v ~a ~{~a=%v~^ ~}\\n"
+		msg vars))
+      (timeNow)
+      ,@vars
+      ))
+  (defun tprint (&key (msg "") vars)
+    "generate go code to print variables in log output with their types"
+    `(fmt.Printf
+      (string
+       ,(format nil "%v ~a ~{~a=%v (%T)~^ ~}\\n"
+		msg vars))
+      (timeNow)
+      ,@(loop for e in vars
+	      appending
+	      `(,e ,e))))
+  (let ((err-nr 0))
+    (defun panic (var-cmd)
+      (let ((err (format nil "err~2,'0d" err-nr)))
+	(destructuring-bind
+	    (&key (var "_") cmd)
+	    var-cmd
+	  (prog1	
+	      `(do0
+		(assign (ntuple ,var ,err)
+			,cmd)
+		(unless (== ,err "nil")
+		  ,(lprint :msg (substitute #\' #\" (emit-go :code cmd))
+			   :vars `(,err))
+		  (panic ,err)))
+	    (incf err-nr)))))
+    (defun panic0 (cmd)
+      (let ((err (format nil "err~2,'0d" err-nr)))
+	(prog1	
+	    `(do0
+	      (assign ,err
+		      ,cmd)
+	      (unless (== ,err "nil")
+		,(lprint :msg (substitute #\' #\" (emit-go :code cmd))
+			   :vars `(,err))
+		(panic ,err))
+	      )
+	  (incf err-nr)))))
+  
+  (let ((file-count 0))
+    (defun write-go (name code)
+      (prog1
+     	  (write-source
+	   (format nil "~a/source~a/~2,'0d_~a"
+		   *path* *idx* file-count name)
+	   code)
+	(incf file-count))))
+
+  (ensure-directories-exist
+   (format nil "~a/source~a/"
+	   *path*
+	   *idx*))
+  
+  ;; overwriting this file makes it necessary to call setup01_tidy again
+  (with-open-file (s (format nil "~a/source~a/go.mod"
+			     *path*
+			     *idx*)
+		     :direction :output
+		     :if-exists nil
+		     :if-does-not-exist :create)
+    (format s "module main~%")
+    (format s "go 1.18~%"))
+  (write-go
+   "main"
+   `(do0
+     (package main)
+     (import
+      ;io
+      ;bufio
+      fmt
+      time
+      ;io/ioutil
+      os
+      ;encoding/binary
+					;github.com/samber/lo
+					;github.com/schollz/progressbar/v3
+	     runtime/pprof
+	     )
+     ,(lprint-init)
+
+     (defstruct0 Greeter
+	 (expression string)
+       (subject string))
+
+     (defmethod greet ((g Greeter))
+       ,(lprint :vars `(g.expression
+			g.subject)))
+     (defun main ()
+       ,(lprint :msg "main")
+
+       (let ((prof_fn (string "satplan.prof")))
+
+	 ;; go tool pprof satpla satplan.prof
+	 ,(lprint :msg "start profiling" :vars `(prof_fn))
+	 ,(lprint :msg "you can view the profile with: go tool pprof satplan satplan.prof")
+	 ,(panic `(:var prof_f
+		   :cmd (os.Create prof_fn)))
+	 (pprof.StartCPUProfile prof_f)
+	 (defer (pprof.StopCPUProfile)))
+
+       (do0
+	(let ((g (curly Greeter
+			:expression (string "hello")
+			:subject (string "you"))))
+	  (g.greet)
+	  #+nil
+	  ,(lprint :vars `(g.expression
+			   g.subject))))
+       ))))
