78fc04c3849c0c1471e759964702d74a9af1b02f
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-08-06 11:16:01 +0200

    webcam example works

---
+++ examples/25_opencv/gen00.lisp
@@ -0,0 +1,116 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-golang-generator"))
+;; https://gocv.io/writing-code/hello-video/
+
+(in-package :cl-golang-generator)
+
+(progn
+  (defparameter *path*
+    (format nil "~a/stage/cl-golang-generator/examples/25_opencv"
+	    (user-homedir-pathname)))
+  (defparameter *idx* "00")
+  (defun lprint-init ()
+    `(defun timeNow ()
+       (declare (values string))
+       (return
+	 (dot time
+	      (Now)
+	      (Format
+	       (string
+		"2006-01-02 15:04:05.000"))))))
+  (defun lprint (&key (msg "") vars)
+    "generate go code to print variables in log output"
+    `(fmt.Printf
+      (string
+       ,(format nil "%v ~a ~{~a=%v~^ ~}\\n"
+		msg (loop for v in vars
+			  collect
+			  (emit-go :code v))))
+      (timeNow)
+      ,@vars
+      ))
+  (defun tprint (&key (msg "") vars)
+    "generate go code to print variables in log output with their types"
+    `(fmt.Printf
+      (string
+       ,(format nil "%v ~a ~{~a=%v (%T)~^ ~}\\n"
+		msg vars))
+      (timeNow)
+      ,@(loop for e in vars
+	      appending
+	      `(,e ,e))))
+  (let ((err-nr 0))
+    (defun panic (var-cmd)
+      (let ((err (format nil "err~2,'0d" err-nr)))
+	(destructuring-bind
+	      (&key (var "_") cmd)
+	    var-cmd
+	  (prog1
+	      `(do0
+		(assign (ntuple ,var ,err)
+			,cmd)
+		(unless (== ,err "nil")
+		  ,(lprint :msg (substitute #\' #\" (emit-go :code cmd))
+			   :vars `(,err))
+		  (panic ,err)))
+	    (incf err-nr)))))
+    (defun panic0 (cmd)
+      (let ((err (format nil "err~2,'0d" err-nr)))
+	(prog1
+	    `(do0
+	      (assign ,err
+		      ,cmd)
+	      (unless (== ,err "nil")
+		,(lprint :msg (substitute #\' #\" (emit-go :code cmd))
+			 :vars `(,err))
+		(panic ,err))
+	      )
+	  (incf err-nr)))))
+
+  (let ((file-count 0))
+    (defun write-go (name code)
+      (prog1
+	  (progn
+     	    (ensure-directories-exist
+	     (format nil "~a/source~2,'0d/"
+		     *path*
+		     file-count))
+	    (with-open-file (s (format nil "~a/source~2,'0d/go.mod"
+				       *path*
+				       file-count)
+			       :direction :output
+			       :if-exists nil
+			       :if-does-not-exist :create)
+	      (format s "module main~%")
+	      (format s "go 1.18~%"))
+	    (write-source
+	     (format nil "~a/source~2,'0d/~a"
+		     *path* file-count name)
+	     code))
+	(incf file-count))))
+
+  (let ((name "getVideo"))
+    (write-go
+     name
+     `(do0
+       (package main)
+       (import
+	fmt
+					;math
+	time
+	gocv.io/x/gocv
+	)
+       ,(lprint-init)
+
+       (defun main ()
+	 ,(lprint :msg (format nil "~a" name))
+	 ,(panic `(:var cam
+			:cmd (gocv.VideoCaptureDevice 0)))
+	 (assign win (gocv.NewWindow (string "hello"))
+		 img (gocv.NewMat))
+	 (for ()
+	      (cam.Read &img)
+	      (win.IMShow img)
+	      (win.WaitKey 1))
+	 
+	   )))))
