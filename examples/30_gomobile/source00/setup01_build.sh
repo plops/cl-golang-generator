34bac5f189d3f58918cc37f1997ec529dea9b872
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-08-13 22:44:33 +0200

    different api

--- examples/30_gomobile/source00/setup01_build.sh
+++ examples/30_gomobile/source00/setup01_build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-gomobile build -v -x -androidapi 19 -target android/arm
+# 19 .. android 4.4
+# 25 .. android 7.1
+# 30 .. android 11
+gomobile build -v -x -androidapi 30 -target android/arm
