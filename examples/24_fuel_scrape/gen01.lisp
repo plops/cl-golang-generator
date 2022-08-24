bf7d380a30d95293457f24661f851cdab950b6b9
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-08-24 23:25:27 +0200

    start writing python code
    but i forgot that on the phone i can't use nbdev

---
+++ examples/24_fuel_scrape/gen01.lisp
@@ -0,0 +1,163 @@
+(eval-when (:compile-toplevel :execute :load-toplevel)
+  (ql:quickload "cl-py-generator")
+  (ql:quickload "alexandria"))
+(in-package :cl-py-generator)
+
+(progn
+  (defparameter *project* "24_fuel_scrape")
+  (defparameter *idx* "01")
+  (defparameter *path* (format nil "/home/martin/stage/cl-golang-generator/examples/~a" *project*))
+  (defparameter *day-names*
+    '("Monday" "Tuesday" "Wednesday"
+      "Thursday" "Friday" "Saturday"
+      "Sunday"))
+  (defun lprint (cmd &optional rest)
+    `(when args.verbose
+       (print (dot (string ,(format nil "{} ~a ~{~a={}~^ ~}" cmd rest))
+                   (format  (- (time.time) start_time)
+                            ,@rest)))))
+
+  (let* ((cli-args `(
+		     (:short "-i" :long "--input" :help "input file" :default (string "fuel.db") :required t)
+		     (:short "-v" :long "--verbose" :help "enable verbose output" :action "store_true" :required nil))))
+    (write-notebook
+     :nb-file (format nil "~a/source~a/~a_view_fuel_db.ipynb" *path* *idx* *idx*)
+     :nb-code
+     `(
+       (python
+	(export
+	 "#|default_exp p01_view_fuel_db"))
+       (python (export
+		(do0
+					;"%matplotlib notebook"
+		 #-nil(do0
+
+		       (imports (matplotlib))
+                                        ;(matplotlib.use (string "QT5Agg"))
+					;"from matplotlib.backends.backend_qt5agg import (FigureCanvas, NavigationToolbar2QT as NavigationToolbar)"
+					;"from matplotlib.figure import Figure"
+		       (imports ((plt matplotlib.pyplot)
+					;  (animation matplotlib.animation)
+					;(xrp xarray.plot)
+				 ))
+
+		       (plt.ion)
+					;(plt.ioff)
+		       ;;(setf font (dict ((string size) (string 6))))
+		       ;; (matplotlib.rc (string "font") **font)
+		       )
+		 (imports (		;os
+					;sys
+			   time
+					;docopt
+					;pathlib
+					(np numpy)
+					;serial
+					(pd pandas)
+					;(xr xarray)
+					;(xrp xarray.plot)
+					;skimage.restoration
+					;(u astropy.units)
+					; EP_SerialIO
+					;scipy.ndimage
+					;   scipy.optimize
+					;nfft
+					;sklearn
+					;sklearn.linear_model
+					;itertools
+					;datetime
+					; (np numpy)
+					;(cv cv2)
+					;(mp mediapipe)
+					;jax
+					; jax.random
+					;jax.config
+					; copy
+					;re
+					;json
+					; csv
+					;io.StringIO
+					;bs4
+					;requests
+			   (sq sqlite3)
+					;(np jax.numpy)
+					;(mpf mplfinance)
+					;selenium.webdriver ;.FirefoxOptions
+			   argparse
+
+			   ))
+
+
+
+
+
+		 (imports-from (matplotlib.pyplot
+				      plot imshow tight_layout xlabel ylabel
+				      title subplot subplot2grid grid
+				      legend figure gcf xlim ylim)
+			       )
+
+		 )
+		))
+       (python
+	(export
+	 (setf start_time (time.time)
+	       debug True)
+	 (setf
+	  _code_git_version
+	  (string ,(let ((str (with-output-to-string (s)
+				(sb-ext:run-program "/usr/bin/git" (list "rev-parse" "HEAD") :output s))))
+		     (subseq str 0 (1- (length str)))))
+	  _code_repository (string ,(format nil "https://github.com/plops/cl-py-generator/tree/master/example/56_myhdl/source/04_tang_lcd/run_04_lcd.py"))
+	  _code_generation_time
+	  (string ,(multiple-value-bind
+			 (second minute hour date month year day-of-week dst-p tz)
+		       (get-decoded-time)
+		     (declare (ignorable dst-p))
+		     (format nil "~2,'0d:~2,'0d:~2,'0d of ~a, ~d-~2,'0d-~2,'0d (GMT~@d)"
+			     hour
+			     minute
+			     second
+			     (nth day-of-week *day-names*)
+			     year
+			     month
+			     date
+			     (- tz)))))
+
+	 (setf start_time (time.time)
+	       debug True)))
+
+       (python
+	(export
+	 (do0 (setf parser (argparse.ArgumentParser))
+	      ,@(loop for e in cli-args
+		      collect
+		      (destructuring-bind (&key short long help default required action) e
+			`(parser.add_argument
+			  (string ,short)
+			  (string ,long)
+			  :default ,(if default
+					default
+					"None")
+			  :help (string ,help)
+					;:required
+			  #+nil
+			  (string ,(if required
+				       "True"
+				       "False"))
+			  :action ,(if action
+				       `(string ,action)
+				       "None"))))
+
+	      (setf args (parser.parse_args)
+		    ))))
+
+       (python
+	(export
+	 ,(lprint "load database" `(args.input))
+	 (setf cnx (sq.connect args.input))
+	 (setf df (pd.read_sql (string "select * from fuel")))
+	 df))))))
+
+
+
