2c72d1d4bc52fe6742aa7676ee1909e71b22edbe
Martin Kielhorn <kielhorn.martin@gmail.com>
2022-09-17 07:24:26 +0200

    define struct

---
+++ examples/38_rest_with_gin/source00/00_main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"runtime"
+	"runtime/debug"
+	"time"
+)
+
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Place  float64 `json:"place"`
+}
+
+func timeNow() string {
+	return time.Now().Format("2006-01-02 15:04:05.000")
+}
+func checkAndPanic(msg string, err error) {
+	if !((err) == (nil)) {
+		fmt.Printf("%v  msg=%v err=%v\n", timeNow(), msg, err)
+		panic(err)
+	}
+}
+func reportDependencies() {
+	bi, ok := debug.ReadBuildInfo()
+	if ok {
+		for _, dep := range bi.Deps {
+			fmt.Printf("%v  dep=%v\n", timeNow(), dep)
+		}
+	} else {
+		fmt.Printf("%v failed to read build info \n", timeNow())
+	}
+}
+func reportGenerator() {
+	code_git_version := "c8a604b7b25a16f9000bae48de9f4fb2357aca07"
+	code_repository := "https://github.com/plops/cl-golang-generator/tree/master/examples/35_rest"
+	code_generation_time := "07:23:57 of Saturday, 2022-09-17 (GMT+1)"
+	fmt.Printf("%v  code_git_version=%v\n", timeNow(), code_git_version)
+	fmt.Printf("%v  code_repository=%v\n", timeNow(), code_repository)
+	fmt.Printf("%v  code_generation_time=%v\n", timeNow(), code_generation_time)
+}
+func main() {
+	fmt.Printf("%v program 00_main starts \n", timeNow())
+	reportGenerator()
+	fmt.Printf("%v Go version: runtime.Version()=%v\n", timeNow(), runtime.Version())
+	reportDependencies()
+}
